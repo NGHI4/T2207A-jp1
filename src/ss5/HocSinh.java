0df32b280cb10c2d7d1e6a62028acb0a7da30b24
NGHI4 <110522520+NGHI4@users.noreply.github.com>
2023-01-06 16:54:22 +0700

    ss44

--- src/ss5/HocSinh.java
+++ src/ss5/HocSinh.java
@@ -1,7 +1,5 @@
 package ss5;
 
-import ss2.HotGirl;
-
 public class HocSinh {
     String ten;
 
@@ -11,56 +9,65 @@ public class HocSinh {
 
     String tenlophoc;
 
-    String diemtrungbinh;
+    double diemtrungbinh;
 
-    public HocSinh() {
+    public HocSinh(String ten,String ngaysinh,String tenlophoc, String diachi, double diemtrungbinh){
+        this.ten = ten;
+        this.ngaysinh = ngaysinh;
+        this.diachi = diachi;
+        this.tenlophoc = tenlophoc;
+        this.diemtrungbinh = diemtrungbinh;
     }
 
-    public HocSinh(String ten, String ngaysinh, String diachi, String tenlophoc, String diemtrungbinh) {
-            this.ten = ten;
-            this.ngaysinh = ngaysinh;
-            this.diachi = diachi;
-            this.tenlophoc = tenlophoc;
-            this.diemtrungbinh = diemtrungbinh;
-        }
-
-    public String getTen() {
+    public String getTen(){
         return ten;
     }
-
-    public void setTen(String ten) {
+    public void setTen(String ten){
         this.ten = ten;
     }
-
-    public String getNgaysinh() {
+    public String getNgaysinh(){
         return ngaysinh;
     }
-
-    public void setNgaysinh(String ngaysinh) {
+    public void setNgaysinh(String ngaysinh){
         this.ngaysinh = ngaysinh;
     }
-
-    public String getDiachi() {
+    public String getDiachi(){
         return diachi;
     }
-
-    public void setDiachi(String diachi) {
+    public void setDiachi(String diachi){
         this.diachi = diachi;
     }
-
-    public String getTenlophoc() {
+    public String getTenlophoc(){
         return tenlophoc;
     }
-
-    public void setTenlophoc(String tenlophoc) {
+    public void setTenlophoc(String tenlophoc){
         this.tenlophoc = tenlophoc;
     }
-
-    public String getDiemtrungbinh() {
+    public double getDiemtrungbinh(){
         return diemtrungbinh;
     }
+    public void setDiemtrungbinh(double diemtrungbinh){
+        diemtrungbinh = diemtrungbinh;
+    }
+    public void baoCaoKetQua(){
+        if(diemtrungbinh < 4){
+            System.out.println("Yeu");
+        }else if(diemtrungbinh < 6.5){
+            System.out.println("Trung Binh");
+        }else if(diemtrungbinh < 8){
+            System.out.println("Kha");
+        }else if(diemtrungbinh < 9.5){
+            System.out.println("Gioi");
+        }else{
+            System.out.println("Xuat Sac");
+        }
+    }
 
-    public void setDiemtrungbinh(String diemtrungbinh) {
-        this.diemtrungbinh = diemtrungbinh;
+    public void inThongTin(){
+        System.out.println("Ho ten:" +ten);
+        System.out.println("Ngay Sinh:" +ngaysinh);
+        System.out.println("Dia Chi:" +diachi);
+        System.out.println("Lop Hoc:" +tenlophoc);
+        System.out.println("Diem TB:" +diemtrungbinh);
     }
 }
--- src/ss5/MainHocSinh.java
+++ src/ss5/MainHocSinh.java
@@ -1,8 +1,68 @@
 package ss5;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class MainHocSinh {
-    public static void main(String[] args){
-        HocSinh x = new HocSinh("Pham Van Nghia","11-10-2004","Ha Noi","T2207A","10");
-        System.out.println(x.getTenlophoc());
+        public static void main(String[] args) {
+            //    HocSinh[] hs = new HocSinh[10];
+            ArrayList<HocSinh> hs = new ArrayList<>();
+            Scanner sc = new Scanner(System.in);
+            for (int i=0;i<10;i++){
+                System.out.println("Nhap ten:");
+                String ten = sc.nextLine();
+                System.out.println("Nhap ngay sinh:");
+                String ns = sc.nextLine();
+                System.out.println("Dia chi:");
+                String dc = sc.nextLine();
+                System.out.println("Lop:");
+                String lh = sc.nextLine();
+                System.out.println("Diem trung binh:");
+                double tb = sc.nextDouble();
+                hs.add(new HocSinh(ten,ns,dc,lh,tb));
+            }
+
+            //    for (int i=0;i<hs.length;i++){
+            //     for (int j=0;j< hs.length-1;j++){
+            //      if (hs[j].getDiemtrungbinh() < hs[j+1].getDiemtrungbinh()){
+            //             HocSinh tmp;
+            //       tmp = hs[j];
+            //         hs[j] = hs[j+1];
+            //          hs[j+1] = tmp;
+            //       }
+            //      }
+            //    }
+            //  for (int i=0;i< hs.length;i++) {
+            //       hs[i].inThongTin();
+            //   }
+            //    for (int i = 0; i<hs.length;i++){
+            ///       if (hs[i].getDtb()>=9.5)
+            //          hs[i].inThongTin();
+            //   }
+            //     for (int i = 0; i<hs.length;i++){
+            //  if (hs[i].getDtb()>=8)
+            //           hs[i].inThongTin();
+            ////   }
+            ///   for (int i = 0; i<hs.length;i++){
+            //      if (hs[i].getDiemtrungbinh()>=6)
+            ///          hs[i].inThongTin();
+            //   }
+            //    for (int i = 0; i<hs.length;i++){
+            //       if (hs[i].getDtb()>=4)
+            //       hs[i].inThongTin();
+            //     }
+            //   for (int i = 0; i<hs.length;i++){
+            //        if (hs[i].getDiemtrungbinh()<4)
+            //           hs[i].inThongTin();
+            //  }
+
+            Collections.sort(hs, new Comparator<HocSinh>(){
+                @Override
+                public int compare(HocSinh o1, HocSinh o2){
+                    return o1.getDiemtrungbinh() - o2.getDiemtrungbinh()>0? 0:-1;
+                }
+            });
+        }
     }
-}
